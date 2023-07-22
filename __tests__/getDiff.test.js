214fbdb0b8267926812be3e62b34f537c66f3aa5
Anastasia R <anawachovski@mail.ru>
2023-07-22 22:17:52 +0300

    add test

---
+++ __tests__/getDiff.test.js
@@ -0,0 +1,13 @@
+import { test, expect } from '@jest/globals';
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import getDiff from '../src/getDiff.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+test('generate difference', () => {
+  expect(getDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(fs.readFileSync('./__fixtures__/resultStylish.txt', 'utf-8'));
+});
