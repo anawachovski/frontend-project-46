a5e83897e532cffc5dac211694926bcb1f840bea
Anastasia R <anawachovski@mail.ru>
2023-07-29 13:23:19 +0300

    update parsers

--- src/parsers.js
+++ src/parsers.js
@@ -1,13 +1,13 @@
 import yaml from 'js-yaml';
 
 const parse = (data, extensionName) => {
-	switch (extensionName) {
-		case 'yml':
-		case 'yaml':
-			return yaml.load(data);
-		default:
-			return JSON.parse(data);
-	}
-}
+  switch (extensionName) {
+    case 'yml':
+    case 'yaml':
+      return yaml.load(data);
+    default:
+      return JSON.parse(data);
+  }
+};
 
 export default parse;
