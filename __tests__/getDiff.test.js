d27bd0d7326c1e99c7218b257f0571917e8eda48
Anastasia R <anawachovski@mail.ru>
2023-08-07 15:44:28 +0300

    fix tests

--- __tests__/getDiff.test.js
+++ __tests__/getDiff.test.js
@@ -8,13 +8,17 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-//const resultExpected = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+const resultExpected = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-test('generate difference', () => {
-	expect(getDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(fs.readFileSync('./__fixtures__/resultStylish.txt', 'utf-8'));
-	expect(getDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'))).toEqual(fs.readFileSync('./__fixtures__/resultStylish.txt', 'utf-8'));
-	expect(getDiff(getFixturePath('file1.json'), getFixturePath('file2.yml'))).toEqual(fs.readFileSync('./__fixtures__/resultStylish.txt', 'utf-8'));
-	expect(getDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain')).toEqual(fs.readFileSync('./__fixtures__/resultPlain.txt', 'utf-8'));
- });
+test.each([
+	{ file1: 'file1.json', file2: 'file2.json', format: undefined, expected: 'resultStylish.txt' },
+	{ file1: 'file1.yml', file2: 'file2.yml', format: undefined, expected: 'resultStylish.txt' },
+	{ file1: 'file1.json', file2: 'file2.json', format: 'stylish', expected: 'resultStylish.txt' },
+	{ file1: 'file1.yml', file2: 'file2.yml', format: 'stylish', expected: 'resultStylish.txt' },
+	{ file1: 'file1.json', file2: 'file2.json', format: 'plain', expected: 'resultPlain.txt' },
+	{ file1: 'file1.yml', file2: 'file2.yml', format: 'plain', expected: 'resultPlain.txt' }
+])('generate difference', ({ file1, file2, format, expected }) => {
+   expect(getDiff(getFixturePath(file1)), getDiff(getFixturePath(file2)), format).toBe(resultExpected(expected));
+});
  
  
