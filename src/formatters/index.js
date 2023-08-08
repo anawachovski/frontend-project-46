2e79b60e7dafb4b3df09d0e7b1f0359169ba5cdd
Anastasia R <anawachovski@mail.ru>
2023-08-08 23:59:38 +0300

    fix json formatt

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,12 +1,11 @@
 import getStylishFormat from './stylish.js';
 import getPlainFormat from './plain.js';
-import getJsonFormat from './json.js';
 
 const getFormat = (filesDif, format) => {
   switch (format) {
     case 'stylish': return getStylishFormat(filesDif);
     case 'plain': return getPlainFormat(filesDif);
-    case 'json': return getJsonFormat(filesDif);
+    case 'json': return JSON.stringify(filesDif, null, '');
     default: throw new Error(`Unknown type: ${format}!`);
   }
 };
--- src/formatters/json.js
+++
@@ -1,3 +0,0 @@
-const getJsonFormat = (tree) => JSON.stringify(tree, null, '');
-
-export default getJsonFormat;
