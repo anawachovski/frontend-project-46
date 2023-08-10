b0a0c4451c128f47d1babe81dba3f9c10c3eec71
Anastasia R <anawachovski@mail.ru>
2023-08-10 23:50:54 +0300

    fix src/index.js

--- src/index.js
+++ src/index.js
@@ -4,14 +4,15 @@ import getFormat from './formatters/index.js';
 import getDiffTree from './getDiff.js';
 import parse from './parsers.js';
 
-const getContent = (filepath) => fs.readFileSync(path.resolve(filepath), 'utf-8');
+const getFullPath = (filepath) => path.resolve(filepath);
+const getContent = (filepath) => fs.readFileSync(getFullPath(filepath), 'utf-8');
 const getExtName = (filepath) => path.extname(filepath).slice(1);
 
 const genDiff = (filepath1, filepath2, format = 'stylish') => {
-  const obje1 = parse(getContent(filepath1), getExtName(filepath1));
+  const obj1 = parse(getContent(filepath1), getExtName(filepath1));
   const obj2 = parse(getContent(filepath2), getExtName(filepath2));
 
-  return getFormat(getDiffTree(obje1, obj2), format);
+  return getFormat(getDiffTree(obj1, obj2), format);
 };
 
 export default genDiff;
